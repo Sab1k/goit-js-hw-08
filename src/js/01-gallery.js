eba904f929c64a2034e387c984dc771d9de72885
Slava Nadtochii <slavanadtochiy@gmail.com>
2022-05-10 13:49:10 +0300

    дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,16 +1,10 @@
-// 1. Добавь библиотеку SimpleLightbox как зависимость проекта 
-// используя npm (ссылка на CDN из твоей прошлой работы больше не нужна).
-// 2. Используй свой JavaScript код из предыдущей домашней работы, но выполни 
-// рефакторинг с учетом того, что библиотека была установлена через npm (синтаксис import/export).
 
-// Для того чтобы подключить CSS код библиотеки в проект, необходимо 
-// добавить еще один импорт, кроме того который описан в документации.
 
 
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
