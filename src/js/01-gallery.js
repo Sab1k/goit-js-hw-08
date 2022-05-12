06ebcc24b407cf8d597701da67a02a533b37e44e
Slava Nadtochii <slavanadtochiy@gmail.com>
2022-05-12 11:35:34 +0300

    дз8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,7 @@
-
-
-
+// import SimpleLightbox from "simplelightbox";
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { galleryItems } from './gallery-items';
 // Change code below this line
